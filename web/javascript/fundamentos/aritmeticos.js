8c0e12a04bce04440c7b6fa1c75af73a4d463de0
HardszVick <connorkenway753@gmail.com>
2020-03-23 14:52:25 -0300

    23/03
    TERMINAR ESSA FUNDAMENTAÇÃO DO JAVASCRIPT QUE PARECE QUE NUNCA ACABA

---
+++ web/javascript/fundamentos/aritmeticos.js
@@ -0,0 +1,14 @@
+const [a,b,c,d] = [3,5,1,15]
+
+const soma = a+b+c+d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d/a
+const modulo = a % 2
+
+//d++ post fixer
+// ++d pre fixer
+//A+b in fixer
+//-a unário
+
+console.log(soma,subtracao,multiplicacao,divisao,modulo)
